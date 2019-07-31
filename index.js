fcf9091b42217792f2d6968108cd0b9b3f085aae
Diana Badas <diana.badas@gmail.com>
2019-07-31 14:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function produceDrivingRange() {
+function produceDrivingRange( blockRange ) {
+  return function ( startingBlock, endingBlock ) {
+    let distanceToTravel = Math.abs( parseInt(endingBlock) - parseInt(startingBlock));
+    let difference = blockRange - distanceToTravel;
 
+    if ( difference > 0 ) {
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
+  }
 }
 
 function produceTipCalculator() {
